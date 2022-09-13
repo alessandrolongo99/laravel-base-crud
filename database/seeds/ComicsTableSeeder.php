03f0b57d5da22e3bc5a0b1bb07d1618160f06018
alessandrolongo99 <longoalessandro153@gmail.com>
2022-09-13 14:41:40 +0200

    added ComicController, ComicModel, ComicsTableSeeder, create_comics_table

---
+++ database/seeds/ComicsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\ComicModel;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $newComic = new ComicModel();
+            $newComic->title = $faker->word;
+            $newComic->author = $faker->userName;
+            $newComic->pageNumber = $faker->numberBetween(100, 200);
+            $newComic->editor = $faker->randomElement(['DC', 'Marvel']);
+        }
+    }
+}
