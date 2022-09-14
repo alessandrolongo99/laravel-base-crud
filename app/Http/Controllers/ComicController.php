63d738497b23ef5c9cfc5983bf7df6d06947c75e
alessandrolongo99 <longoalessandro153@gmail.com>
2022-09-14 15:12:36 +0200

    corrente assente

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -15,7 +15,7 @@ class ComicController extends Controller
     public function index()
     {
         $comics = Comic::all();
-        return view('welcome', compact('comics'));
+        return view('create', compact('comics'));
     }
 
     /**
---
+++ resources/views/create.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="text-center mt-5">CREATE NEW ELEMENT</h1>
+        <form action="{{ route('comics.store') }}" method="POST">
+            @csrf
+            <div class="mb-3">
+                <label for="title" class="form-label">Titolo</label>
+                <input type="text" class="form-control" name="title" id="title">
+            </div>
+            <div class="mb-3">
+                <label for="description" class="form-label">Descrizione</label>
+                <input type="text" class="form-control" name="description" id="description">
+            </div>
+            <div class="mb-3">
+                <label for="thumb" class="form-label">Copertina</label>
+                <input type="url" class="form-control" name="thumb" id="thumb">
+            </div>
+            <div class="mb-3">
+                <label for="price" class="form-label">Prezzo</label>
+                <input type="number" class="form-control" name="price" id="price">
+            </div>
+            <div class="mb-3">
+                <label for="series" class="form-label">Serie</label>
+                <input type="text" class="form-control" name="series" id="series">
+            </div>
+            <div class="mb-3">
+                <label for="thumb" class="form-label">Copertina</label>
+                <input type="url" class="form-control" name="thumb" id="thumb">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</body>
+
+</html>
